fc1650db1159c37ca193edb4e19ec009f9cd2b07
Caio Brandão <40217603+A0Fz@users.noreply.github.com>
2019-04-02 16:03:49 -0300

    searchbar-funcional#1

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular'
+import { MusicService } from '../services/music.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: 'list.page.html',
   styleUrls: ['list.page.scss']
 })
+
+
 export class ListPage implements OnInit {
+
+  private music_name:string;
+  private artist_name:string;
+  private album_name:string;
   
-  constructor() {}
+  constructor(private loadingController: LoadingController) {}
 
   ngOnInit() {
-    
+    this.consultaAlbum()
+    this.consultaMusica()
+    this.consultaArtista()
   }
+  async consultaMusica(index?){
+    //loading
+    const loading = await this.loadingController.create({
+      message: 'Carregando Músicas...'
+    });
+  }
+
+    async consultaAlbum(index?){
+      //loading
+      const loading = await this.loadingController.create({
+        message: 'Carregando albuns...'
+      });
+    }
+      async consultaArtista(index?){
+        //loading
+        const loading = await this.loadingController.create({
+          message: 'Carregando Artistas...'
+        });
+  
+      }
+    
   
 }
--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -3,7 +3,9 @@ import { HttpHeaders, HttpClient  } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { Observable,of } from 'rxjs';
 
-const URL_API = "http://localhost:3000/music";
+const URL_M = "http://localhost:3000/musicas";
+const URL_A = "http://localhost:3000/artista";
+const URL_B= "http://localhost:3000/album";
 const httpOptions ={
   headers: new HttpHeaders({ 'Content-Type' : 'application/json;charset=utf-8'})
 };
@@ -17,19 +19,19 @@ export class MusicService {
   constructor(private htttp: HttpClient) { }
 
   getMusic(param:string = ""): any {
-    return this.htttp.get<any>(` ${URL_API}${param}`, httpOptions).pipe(
+    return this.htttp.get<any>(` ${URL_M}${param}`, httpOptions).pipe(
       catchError(this.handleError<any>('Falha ao recuperar a música'))
     )
   }
 
   getArtist(param:string = ""): any {
-    return this.htttp.get<any>(` ${URL_API}${param}`, httpOptions).pipe(
+    return this.htttp.get<any>(` ${URL_A}${param}`, httpOptions).pipe(
       catchError(this.handleError<any>('Falha ao recuperar Artista/Banda'))
     )
   }
 
   getAlbum(param:string = ""): any {
-    return this.htttp.get<any>(` ${URL_API}${param}`, httpOptions).pipe(
+    return this.htttp.get<any>(` ${URL_B}${param}`, httpOptions).pipe(
       catchError(this.handleError<any>('Falha ao recuperar o Albúm'))
     )
   }
